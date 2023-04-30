b533ee558896d1c5cca3659fe95465b9c7de473b
hyoin <kslove1111@gmail.com>
2023-04-30 17:17:58 +0900

    등록/수정/조회 API 만들기

---
+++ src/main/java/com/hi/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.hi.book.springboot.web.dto;
+
+import com.hi.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
